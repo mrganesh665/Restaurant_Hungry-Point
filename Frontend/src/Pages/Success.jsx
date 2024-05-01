5287a9abbdb60d5a3e97bc93effe5e7f22802bba
Ganesh Chaurasiya <mrganesh665@gmail.com>
2024-05-02 00:43:47 +0530

    last commit

--- Frontend/src/Pages/Success.jsx
+++ Frontend/src/Pages/Success.jsx
@@ -1,17 +1,3 @@
-// import React from 'react'
-
-// const Success = () => {
-//     return (
-//         <>
-//             <h1>Success Page </h1>
-//             pp
-//         </>
-//     );
-// }
-
-// export default Success
-
-
 import React from 'react';
 import { Link } from 'react-router-dom'; // If using React Router for navigation
 import './Success.css'; // Import CSS file
@@ -19,17 +5,14 @@ import './Success.css'; // Import CSS file
 const Success = () => {
     return (
         <div className="container">
-             <h3> HUNGRY POINT </h3>
+            <h3> HUNGRY POINT </h3>
             <h1 className="heading">Success!</h1>
             <p className="message">Your Table is reserved</p>
             <div className="details">
                 <p className="detailItem"><strong>Confirmation Number:</strong> ABC123</p>
-                {/* Add more relevant details here */}
             </div>
-            {/* <p className="info">A confirmation email has been sent to your inbox. Please check your email shortly.</p> */}
             <div className="actions">
                 <Link to="/" className="link">Go Back to Home</Link>
-                {/* Add more actions/buttons as needed */}
             </div>
         </div>
     );
